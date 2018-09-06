38f8a27e238a47394174f2e1886e1913d16ffb42
cerise612 <developerdanielle12@gmail.com>
2018-09-06 07:24:30 -0700

    .

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 // pretty-print
 
-require("dotenv").config();
+var dotenv = require("dotenv").config();
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var keys = require("./keys");
@@ -9,114 +9,136 @@ var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var inquirer = require("inquirer");
 
+var command = process.argv[2];
+
 // user prompt
-inquirer
-  .prompt([
-    {
-        type: "list",
-        name: "doingWhat",
-        message: "What Action Would You Like To Perform?",
-        choices: [
-          "my-tweets",
-          "spotify-this-song",
-          "movie-this",
-        ]
-    },
-  ]).then(function(res) {
-    switch (res.doingWhat) {
-    case res.doingWhat === "my-tweets":
-      getMyTweets();
-      break;
-    case res.doingWhat === "spotify-this-song":
-      getMeSpotify(functionData);
-      break;
-    case res.doingWhat === "movie-this":
-      getMeMovie(functionData);
-      break;
-    default:
-      console.log("LIRI doesn't know that");
-  };
-  });
+// inquirer
+//   .prompt([
+//     {
+//         type: "list",
+//         name: "doingWhat",
+//         message: "What Action Would You Like To Perform?",
+//         choices: [
+//           "my-tweets",
+//           "spotify-this-song",
+//           "movie-this",
+//         ]
+//     },
+//   ]).then(function(res) {
+//     switch (res.doingWhat) {
+//     case res.doingWhat === "my-tweets":
+//       getMyTweets();
+//       break;
+//     case res.doingWhat === "spotify-this-song":
+//       getMeSpotify(functionData);
+//       break;
+//     case res.doingWhat === "movie-this":
+//       getMeMovie(functionData);
+//       break;
+//     default:
+//       console.log("LIRI doesn't know that");
+//   };
+//   });
 
 // Writes to the log.txt file
-var getArtistNames = function(artist) {
-  return artist.name;
-};
+// var getArtistNames = function(artist) {
+//   return artist.name;
+// };
 
 // Function for running a Spotify search
-var getMeSpotify = function(songName) {
-  if (songName === undefined) {
-    songName = "Redbone";
-  }
+var getMeSpotify = function(query) {
+   var spotify = new Spotify(keys.spotify);
 
-  spotify.search(
-    {
-      type: "track",
-      query: songName
-    },
-    function(err, data) {
-      if (err) {
-        console.log("Error occurred: " + err);
-        return;
-      }
-
-      var songs = data.tracks.items;
-
-      for (var i = 0; i < songs.length; i++) {
-        console.log(i);
-        console.log("artist(s): " + songs[i].artists.map(getArtistNames));
-        console.log("song name: " + songs[i].name);
-        console.log("preview song: " + songs[i].preview_url);
-        console.log("album: " + songs[i].album.name);
-        console.log("-----------------------------------");
-      }
+    if (query === "undefined") {
+        query = "Welcome To The Jungle";
     }
-  );
-};
 
+    spotify.search({ type: 'track', query: query }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var song = data.tracks.items[0];
+
+        stringSong = "\r\nspotify-this-song\r\nArtist: " + song.album.artists[0].name + "\r\nSong Name: " + song.name + "\r\nPreview URL: " + song.preview_url + "\r\nAlbum: " + song.album.name
+        console.log(stringSong);
+
+        fs.appendFile("log.txt", stringSong, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+
+    });
+
+}
 // Function for running a Twitter Search
 var getMyTweets = function() {
-  var client = new Twitter(keys.twitter);
-
-  var params = {
-    screen_name: "athousandstrangeplaces"
-  };
-  client.get("statuses/user_timeline", params, function(error, tweets, response) {
-    if (!error) {
-      for (var i = 0; i < tweets.length; i++) {
-        console.log(tweets[i].created_at);
-        console.log("");
-        console.log(tweets[i].text);
-      }
-    }
-  });
-};
+ var client = new Twitter(keys.twitter);
+    client.get('statuses/user_timeline', { screen_name: 'a1000strngeplce', count: 20 }, function (error, tweets, response) {
+        fs.appendFile("log.txt", "my-tweets command:\r", function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+        for (x in tweets) {
+            console.log(tweets[x].text);
+            fs.appendFile("log.txt", tweets[x].text + "\r", function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+        }
+    });
+}
 
 // Function for running a Movie Search
-var getMeMovie = function(movieName) {
-  if (movieName === undefined) {
-    movieName = "Mrs Doubtfire";
-  }
-
-  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
-
-  request(urlHit, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var jsonData = JSON.parse(body);
-
-      console.log("Title: " + jsonData.Title);
-      console.log("Year: " + jsonData.Year);
-      console.log("Rated: " + jsonData.Rated);
-      console.log("IMDB Rating: " + jsonData.imdbRating);
-      console.log("Country: " + jsonData.Country);
-      console.log("Language: " + jsonData.Language);
-      console.log("Plot: " + jsonData.Plot);
-      console.log("Actors: " + jsonData.Actors);
-      console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+var getMeMovie = function(query) {
+    if (query === "undefined") {
+        query = "Mr Nobody";
     }
-  });
+    request("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+
+            data = JSON.parse(body);
+            stringMovie = "\r\nmovie-this\r\nTitle: " + data.Title + "\r\nYear: " + data.Year + "\r\nIMDB Rating: " + data.Ratings[0].Value + "\r\nRotten Tomatoes Rating: " + data.Ratings[1].Value + "\r\nProduced in: " + data.Country + "\r\nLanguage: " + data.Language + "\r\nPlot: " + data.Plot + "\r\nActors: " + data.Actors
+            console.log(stringMovie);
+            fs.appendFile("log.txt", stringMovie, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+        }
+    });
 };
 
+var doWhat = function () {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data);
+        var dataArr = data.split(",");
+        console.log(dataArr);
 
+    });
+};
+
+if (process.argv.length > 3) {
+    var query = process.argv.slice(3).join(" ");
+} else {
+    query = "undefined"
+}
+if (command === "my-tweets") {
+    getMeSpotify();
+} else if (command === "spotify-this-song") {
+    getMyTweets(query);
+} else if (command === "movie-this") {
+    getMeMovie(query);
+} else if (command === "do-what-it-says") {
+    doWhat(query);
+} else {
+    console.log("Invalid command");
+}
 
 
